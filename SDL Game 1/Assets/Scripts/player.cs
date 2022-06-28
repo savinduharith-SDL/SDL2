d543ff5515bfdef9b3512f5f9ea9af4833991490
Savindu Harith <sharitecdigitallabs@gmail.com>
2022-06-28 17:15:56 +0530

    6-27 commit

---
+++ SDL Game 1/Assets/Scripts/player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    Rigidbody rbPlayer;
+    [SerializeField]float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalalInput = Input.GetAxis("Vertical");
+        Vector3 v3 = new Vector3(horizontalInput * speed, 0f, verticalalInput * speed);
+        rbPlayer.AddForce(v3);
+    }
+}
